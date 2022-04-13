e998ad61261888b7eb8e1c5b63b0276959845c67
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2022-04-13 21:28:43 +0530

    Adds flatlist for users from sample API

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  const uri = "https://reqres.in/api/users?page=1"
+
+  useEffect(() => {
+    fetch(uri)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false))
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <FlatList
+          data={data.data}
+          keyExtractor={item => item.id}
+          contentContainerStyle={{
+            padding: 10,
+            paddingTop: Constants.statusBarHeight+30 || 42
+          }}
+          renderItem={({ item, index }) => {
+            return <View style={{
+              flexDirection: 'row',
+              marginBottom: 25,
+              padding: 10,
+              backgroundColor: 'white',
+              marginHorizontal: 20,
+              borderRadius: 50
+              
+            }}>
+              <Image
+                source={{ uri: item.avatar }}
+                style={{
+                  width: 70,
+                  height: 70,
+                  borderRadius: 70,
+                  borderWidth: 2,
+                  borderColor: 'gray',
+                  marginRight: 30
+                }}
+              />
+              <View>
+                <Text style={{fontSize: 20, fontWeight: 'bold'}}>{item.first_name + ' ' + item.last_name}</Text>
+                <Text style={{fontSize: 15, color: 'blue'}}>{item.email}</Text>
+              </View>
+            </View>
+          }}
+        />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#EBEDEF',
   },
 });
